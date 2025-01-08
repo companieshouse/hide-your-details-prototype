46a50ec55896fc8a342b0711f87b071686d763e2
mtomas-ch <119611289+mtomas-ch@users.noreply.github.com>
2025-01-08 11:50:40 +0000

    v3 changes added

--- app/routes/v3-routes.js
+++ app/routes/v3-routes.js
@@ -389,11 +389,46 @@ router.post('/v3/need-replacement-address', function (req, res) {
       errorList: errors
     })
   } else {
-    if (req.session.data['needReplacementAddress'] === 'Yes') {
-      res.redirect('/v3/replacement-address-lookup')
+    if (req.session.data['needReplacementAddress'] === 'yes') {
+      res.redirect('/v3/is-address-roa')
     } else {
       // User inputted value so move to next page
       res.redirect('/v3/select-documents')
+    }
+  }
+})
+
+// ******* is-address-roa javascript ********************************
+router.get('/v3/is-address-roa', function (req, res) {
+  // Set URl
+  res.render('v3/is-address-roa', {
+    currentUrl: req.originalUrl
+  })
+})
+
+router.post('/v3/is-address-roa', function (req, res) {
+  // Create empty array
+  var errors = []
+
+  // Check if user has filled out a value
+  if (typeof req.session.data['isAddressRoa'] === 'undefined') {
+    // No value so add error to array
+    errors.push({
+      text: 'Select yes if the home address is the registered office address',
+      href: '#isAddressRoa'
+    })
+
+    // Re-show page with error value as true so errors will show
+    res.render('v3/is-address-roa', {
+      errorIsAddressRoa: true,
+      errorList: errors
+    })
+  } else {
+    if (req.session.data['isAddressRoa'] === 'no') {
+      res.redirect('/v3/replacement-address-lookup')
+    } else {
+      // User inputted value so move to next page
+      res.redirect('/v3/address-roa-stop')
     }
   }
 })
